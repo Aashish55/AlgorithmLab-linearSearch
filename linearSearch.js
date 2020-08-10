90cbd350dda8b26bb6f101cd58275d816620caec
Aashish Jangam <38790510+Aashish55@users.noreply.github.com>
2020-08-10 06:11:05 +0545

    execution time added

--- linearSearch.js
+++ linearSearch.js
@@ -1,21 +1,29 @@
-function linear() {
-    var n = parseInt(prompt("Enter the size of an array"));
-    var a = new Array(n);
-    var t = 0;
-    for (var i = 0; i < a.length; i++) {
-        a[i] = parseInt(prompt("Enter array elements"));
+function linear_search(arr, x) {
+    // Go through all the elements of arr to look for item.
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === x) return `INDEX of ${x} is: ${i}`;
     }
-    var k = parseInt(prompt("Enter the key element to search: "));
-    for (var i = 0; i < a.length; i++) {
-        if (k == a[i]) {
-            t = 1;
-            break;
-        }
-    }
-    if (t == 1) {
-        document.writeln("Element " + a[i] + " Found at Position:" + i);
-    }
-    else {
-        document.writeln("Element Not Found");
-    }
+    // Item not found in the array.
+    return `${x} cannot be found!`;
 }
+//numerical search
+var arr = [1, 3, 7, 11, 19, 23, 29, 37, 43, 56, 61, 67, 71, 79, 83, 97]
+
+console.log(linear_search(arr, 67))
+console.log(linear_search(arr, 89))
+//character search
+arr = ['a', 'e', 'b', 'd', 'g', 'z'];
+console.log(linear_search(arr, 'b'));
+console.log(linear_search(arr, 'f'));
+//running time
+let start = Date.now();
+x = 67
+linear_search([1, 3, 7, 11, 19, 23, 29, 37, 43, 56, 61, 67, 71, 79, 83, 97], x)
+let end = Date.now()
+console.log(`Linear Searching time taken for ${x} is: ${end - start} ms.`)
+//random data running time
+start = Date.now();
+data = [...Array(1000000).keys()];
+linear_search(data, 10000)
+end = Date.now()
+console.log(`Linear Searching time taken is:: ${end - start} ms.`)
